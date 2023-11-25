6b32cc93b215eab605101ee81e2954c291a0cc96
Your Name <galnoelpeterrindengan@gmail.com>
2023-11-25 15:05:02 +0800

    feat: reservation

---
+++ database/migrations/2023_11_25_061747_create_reservations_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('customer_id')
+                ->nullable()
+                ->constrained(
+                    table: 'customers',
+                    indexName: 'reservation_customer_id'
+                )
+                ->onUpdate('cascade')
+                ->onDelete('set null');
+
+            $table->foreignId('table_id')
+                ->nullable()
+                ->constrained(
+                    table: 'tables',
+                    indexName: 'reservation_table_id'
+                )
+                ->onUpdate('cascade')
+                ->onDelete('set null');
+            
+            $table->string('status');
+            $table->integer('number_of_people');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
