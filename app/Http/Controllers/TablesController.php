f1d096d57a590ce311e2466a251abcad9e32d574
@thyrawynne <thyra.wynne.mandak10@gmail.com>
2023-11-29 15:55:36 +0800

    added a TablesController data

--- app/Http/Controllers/TablesController.php
+++ app/Http/Controllers/TablesController.php
@@ -63,4 +63,39 @@ class TablesController extends Controller
     {
         //
     }
+
+    function tableStatusForWaiters() {
+        $tables = tables::all();
+        $tableStatusForWaiters = [];
+        foreach ($tables as $table) {
+            if ($table->status === 'pending') {
+                $waiter = $table->waiter;
+            if ($waiter){
+                $tableStatusForWaiters[$waiter->id][] = [
+                    'table_type' => $table->table_type, 
+                    'status' => $table->status,
+                    'customer_name' => $table->customer_name, 
+                    'update_button' => true, 
+                ];
+            }  
+        }
+    }
+
+        return $tableStatusForWaiters;
+}
+    
+    function updateTableStatus($request, tables $tables) {
+        $tableType = $request->input('table_type');
+        $updatedStatus = $request->input('status'); 
+
+        $table = tables::where('table_type', $tableType)->first();
+    
+        if ($table) {
+            if ($updatedStatus === 'confirm' || $updatedStatus === 'reject') {
+                $table->status = $updatedStatus;
+                $table->save();
+            } else {
+            }
+        }
+    }
 }
